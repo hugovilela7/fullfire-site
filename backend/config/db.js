af685cef9d1ed4cf096b2b530ac31a9f2dca735e
Hugo Vilela <hugogabrielvilelas@gmail.com>
2025-11-28 17:46:14 -0300

    Atualização do WhatsApp e ajustes no site

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('fullfire', 'postgres', 'Rezendeev!l123', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
